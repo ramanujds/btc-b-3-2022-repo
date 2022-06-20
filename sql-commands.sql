6726e6d6846a20f4371a9265e03859facf37983c
Ramanuj DS <ramanujds9@gmail.com>
2022-06-20 14:59:38 +0530

    subqueries

--- sql-commands.sql
+++ sql-commands.sql
@@ -52,6 +52,81 @@ select * from emp where extract(year from hiredate)<1981;
 
 select * from emp where mgr in( select distinct mgr from emp where mgr is not null);
 
+-- TCL Commands --
+use btc_db3;
+show tables;
+
+select * from student;
+
+-- ACID --
+-- GROUPPING of Rows --
+-- Group Functions --
+-- max, min, count, avg, sum --
+
+select deptno, min(sal) as 'Min Salary' from emp where deptno=20 group by deptno;
+
+-- Find the total employees belonging to dept no 20 --
+
+select count(ename) from emp where deptno=20;
+
+-- Find the total employees belonging to each dept --
+
+select  deptno, count(ename) as 'Total Employees' from emp group by deptno;
 
 
+-- where -> group by -> having -> order by --
 
+-- JOINS and Subqueries --
+-- Outer Joins : Left Join, Right Join, Full Join --
+-- Inner Joins : --
+-- Self Join --
+-- Cross Join --
+
+create table dept(deptno bigint(4), deptname varchar(50), location varchar(50));
+
+insert into dept values(10,'IT','Bangalore');
+insert into dept values(20,'Sales','Chennai');
+insert into dept values(30,'Accounts','Delhi');
+insert into dept values(40,'Management','Kolkata');
+
+commit;
+
+select * from dept;
+
+select e.*, d.* from emp e left join dept d on e.deptno=d.deptno;
+
+-- find the dept which doesn't have any employee --
+select d.* from emp e RIGHT join dept d on e.deptno=d.deptno where e.empno is NULL;
+
+-- Find the employees who are working in Delhi --
+
+-- Find the total employees belonging to each dept where two or more employees are working --
+select deptno,count(*) from emp group by deptno having count(*)>=2;
+
+-- Find the details of each dept where two or more employees are working --
+
+
+-- Find the dept, average salary, max salary, min salary where min salary>2000 --
+
+select deptno,avg(sal),max(sal),min(sal) from emp group by deptno having min(sal)>900;
+
+-- Display the details of the employees who are working in IT Dept. --
+select * from emp join dept  using(deptno) where deptname='IT';
+
+select * from emp where deptno = (select deptno from dept where deptname='IT');
+
+-- Count the employees working under each manager --
+
+select e1.empno, e1.ename, count(*) as 'Total Employees' from emp e1 join emp e2 on e1.empno=e2.mgr group by e1.empno,e1.ename;
+
+select e1.empno, e1.ename from emp e1 join emp e2 on e1.empno=e2.mgr;
+
+-- Subqueries --
+
+-- show dept details with total employee count --
+
+select d.deptno, d.deptname, count(e.empno) as 'Total Employees' from emp e right join dept d using(deptno) group by d.deptno, d.deptname;
+
+
+-- co-related subquery --
+select d.*, (select count(empno) from emp e where e.deptno=d.deptno) as 'Total Employees' from dept d;
