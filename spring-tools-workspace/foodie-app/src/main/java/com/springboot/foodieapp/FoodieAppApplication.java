db7e854d048d9a86279543efd8f6efdd474bc51e
Ramanuj DS <ramanujds9@gmail.com>
2022-07-01 14:35:33 +0530

    Spring REST Api

---
+++ spring-tools-workspace/foodie-app/src/main/java/com/springboot/foodieapp/FoodieAppApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.foodieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodieAppApplication.class, args);
+	}
+
+}
---
+++ spring-tools-workspace/foodie-app/src/test/java/com/springboot/foodieapp/FoodieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.foodieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
